ae6bb6ce4281181b6a60d719e17fab2f08ae4cb3
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-10 20:54:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
 
 // Write your code here and print the output using alert function
+let num1 = +input1
+let num2 = +input2
+if(num1 && num2){
+	alert(`The sum of ${num1} and ${num2} is ${num1+num2}.`)
+}else{
+	alert("Invalid input. Please enter a valid number.")
+}
+
+
+
